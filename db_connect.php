cf742dab038afded935551938c572507f773afde
hanpkaria <144020616+hanpkaria@users.noreply.github.com>
2024-08-23 12:42:50 +0200

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -4,12 +4,10 @@ $username = "root";
 $password = "";
 $dbname = "webshop";
 
-// Establishing a connection using object-oriented style
 $conn = new mysqli("localhost", "root", "", "webshop");
 
 
-// Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-// echo "Connected succesfully";
+// echo "Geconnecteerd";
