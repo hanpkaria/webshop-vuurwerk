0829da55d9df4c5d2ab05a379270e637c823a64e
hanpkaria <144020616+hanpkaria@users.noreply.github.com>
2024-08-23 12:41:32 +0200

    Update add_product.php

--- add_product.php
+++ add_product.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nieuw Product Toevoegen</title>
     <style>
-        /* Voeg hier eventuele CSS-stijlen toe */
         body {
             font-family: Arial, sans-serif;
             background-color: #f0f0f0;
